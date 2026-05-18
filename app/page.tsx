779b33e6e239c24f46ef6ab978caeb7a8f664792
github_dominikus02-source <dominikus.02@gmail.com>
2026-05-18 22:12:37 +0700

    remove pricing section from landing page and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import AIFeatures from "@/components/landing/AIFeatures";
 import CommunitySection from "@/components/landing/CommunitySection";
 import MarketplaceSection from "@/components/landing/MarketplaceSection";
 import Testimonials from "@/components/landing/Testimonials";
-import PricingTable from "@/components/landing/PricingTable";
 import FAQSection from "@/components/landing/FAQSection";
 import FinalCTA from "@/components/landing/FinalCTA";
 import PublicNavbar from "@/components/public/PageNavbar";
@@ -245,7 +244,6 @@ export default async function HomePage() {
       <CommunitySection />
       <MarketplaceSection />
       <Testimonials />
-      <PricingTable />
       <FAQSection />
       <FinalCTA />
       
